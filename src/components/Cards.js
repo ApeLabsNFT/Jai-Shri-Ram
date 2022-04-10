1d8086c0ad0f576e996b80cfc552826ec24d87e2
Swastik Dwivedi <dwivediswastik@gmail.com>
2022-04-10 18:47:29 +0530

    working nft

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -5,7 +5,7 @@ function Cards() {
   return (
     <div class="row">
     <div class="column">
-      <div class="card">
+      <div class="card2">
         <h1>Total Supply</h1>
         <br></br>
       <p>1000</p>
@@ -13,7 +13,7 @@ function Cards() {
       </div>
     </div>
     <div class="column">
-      <div class="card"> 
+      <div class="card2"> 
       <h1>Holders</h1>
       <br></br>
       <p>$0.01</p>
@@ -22,7 +22,7 @@ function Cards() {
     </div>
     <div class="column">
 
-      <div class="card">
+      <div class="card2">
       <h1 >Volume</h1>
       <br></br>
       <p>$0.01</p>
@@ -30,7 +30,7 @@ function Cards() {
       </div>
     </div>
     <div class="column">
-      <div class="card"> <h1>$COIN/USDC</h1>
+      <div class="card2"> <h1>$COIN/USDC</h1>
       <br></br>
       <p>$0.01</p>
       <br></br>
