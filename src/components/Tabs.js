978b0d9aa1a736b90135ae421376dd1b5edaa11b
Swastik Dwivedi <dwivediswastik@gmail.com>
2022-04-10 18:44:13 +0530

    working nft

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -10,32 +10,46 @@ function Tabs(){
   <ul class="cards">
   <li>
     <span class="card">
-      <img src={NFT27} class="card__image" alt="" />
+      <img src={NFT15} class="card__image" alt="" />
       <div class="card__overlay">
         <div class="card__header">
           <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                     
          
           <div class="card__header-text">
-            <h3 class="card__title">RAM JI</h3>            
+            <h3 class="card__title">Ram Avatar #15</h3>            
             <span class="card__status">1 hour ago</span>
           </div>
         </div>
-        <p class="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
+        <p class="card__description">Attributes:<br></br> <span class="trait">BACKGROUND : SunriseYellow,
+        <br></br>BODY: Yudhawas,<br></br>
+        JEWELLERY: SadhuElite,<br></br>
+        VASTRA: RamVastraVanwas,<br></br>
+        BOWS: WOOD,<br></br>
+        ARROWS: DSHAPED,<br></br>
+        COMPILER: Ape Labs<br></br>
+        </span></p>
       </div>
     </span>      
   </li>
   <li>
     <span class="card">
-      <img src={NFT15} class="card__image" alt="" />
+      <img src={NFT27} class="card__image" alt="" />
       <div class="card__overlay">        
         <div class="card__header">
           <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                 
           <div class="card__header-text">
-            <h3 class="card__title">RAM JI</h3>
+            <h3 class="card__title">RAM AVATAR #27</h3>
             <span class="card__status">3 hours ago</span>
           </div>
         </div>
-        <p class="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
+        <p class="card__description">Attributes:<br></br> <span class="trait">BACKGROUND : Purple,
+        <br></br>BODY: Yudhawas,<br></br>
+        JEWELLERY: SadhuElite,<br></br>
+        VASTRA: Attire1,<br></br>
+        BOWS: GOLDEN,<br></br>
+        ARROWS: Interceptor,<br></br>
+        COMPILER: Ape Labs<br></br>
+        </span></p>
       </div>
     </span>
   </li>
@@ -47,12 +61,19 @@ function Tabs(){
           <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                     
          
           <div class="card__header-text">
-            <h3 class="card__title">RAM JI</h3>
+            <h3 class="card__title">RAM AVATAR #18</h3>
             
             <span class="card__status">1 hour ago</span>
           </div>
         </div>
-        <p class="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
+        <p class="card__description">Attributes:<br></br> <span class="trait">BACKGROUND : MintyGreen,
+        <br></br>BODY: Bharmhawas,<br></br>
+        JEWELLERY: Manohara,<br></br>
+        VASTRA: Attire8,<br></br>
+        BOWS: WOOD,<br></br>
+        ARROWS: KissOfJolt,<br></br>
+        COMPILER: Ape Labs<br></br>
+        </span></p>
       </div>
     </span>
   </li>
@@ -64,11 +85,18 @@ function Tabs(){
           <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                 
           
           <div class="card__header-text">
-            <h3 class="card__title">RAM JI</h3>
+            <h3 class="card__title">RAM AVATAR #9</h3>
             <span class="card__status">3 hours ago</span>
           </div>          
         </div>
-        <p class="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
+        <p class="card__description">Attributes:<br></br> <span class="trait">BACKGROUND : PastelOrange,
+        <br></br>BODY: Bharmhawas,<br></br>
+        JEWELLERY: SadhuElite,<br></br>
+        VASTRA: Attire9,<br></br>
+        BOWS: Gandiva,<br></br>
+        ARROWS: KissOfJolt,<br></br>
+        COMPILER: Ape Labs<br></br>
+        </span></p>
       </div>
     </span>
   </li>    
