e795eff0401d82d5ca33ae3fa4cb30795c64aeda
drunckj <dwivediswastik@gmail.com>
2022-04-14 19:19:36 +0530

    new look for phone

--- src/components/parrallax.js
+++ src/components/parrallax.js
@@ -4,7 +4,7 @@ import RAMMODEL from "../assets/Asset 53.png"
 function Parrallax(){
     return(<><div class="containerp">
     <div class="texts">
-      <h1 class='rellaxh' data-rellax-speed='8'>Hum Sabme Base hai Shri Ram</h1>
+      <h1 class='rellaxh' data-rellax-speed='8'>Hum Sabme Base Hai Shri Ram</h1>
       <p class='rellaxp' data-rellax-speed='5'>Rāma is a Vedic Sanskrit word with two contextual meanings. In one context as found in Atharva Veda, as stated by Monier Monier-Williams, means "dark, dark-colored, black" and is related to the term ratri which means night. In another context as found in other Vedic texts, the word means "pleasing, delightful, charming, beautiful, lovely".</p>
     </div>
     <div class="imgp">
