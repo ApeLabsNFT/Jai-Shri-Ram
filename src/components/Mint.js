909b39753ebd6c7f38245cdd22f00a4d2a867e57
drunckj <dwivediswastik@gmail.com>
2022-04-23 17:19:05 +0530

    new mint css

--- src/components/Mint.js
+++ src/components/Mint.js
@@ -1,22 +1,21 @@
-import React, { useState } from "react";
 import "./mint.css"
 import NFT11 from"../assets/32.png"
-import Mintscreen from "./Mintscreen";
+
+import Connect from "../connect";
 function Mint(){
-    const [show]=useState(true)
     return(<>
         
         <div class="mintco">
         <h1  className="mintcon">"  MINT  "</h1>     
         <div class="fullco">
-        <img class="mintim" src={NFT11}/>
+        <img class="mintim" src={NFT11} alt="jaishriram"/>
             <div class="mintimage">
                 
-            <Mintscreen/>
+            <Connect/>
 
 
             </div>
-            <img class="mintim2" src={NFT11}/>
+            <img class="mintim2" src={NFT11}  alt="jaishriram"/>
             </div>
             
         </div>
